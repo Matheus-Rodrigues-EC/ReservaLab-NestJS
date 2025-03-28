a761d510eb607cab53963f1fcf76f36f5a7ce1f2
Matheus Rodrigues <68609506+Matheus-Rodrigues-EC@users.noreply.github.com>
2025-03-28 12:07:01 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [DatabaseModule, AuthModule, UsersModule],
+})
+export class AppModule {}
