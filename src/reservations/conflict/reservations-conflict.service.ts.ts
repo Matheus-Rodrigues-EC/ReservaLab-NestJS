cdd666d0d2572761d0938ed93c62978d9c9c864a
Matheus Rodrigues <68609506+Matheus-Rodrigues-EC@users.noreply.github.com>
2025-04-12 23:25:26 -0300

    fix: Removed consoles

--- src/reservations/conflict/reservations-conflict.service.ts.ts
+++ src/reservations/conflict/reservations-conflict.service.ts.ts
@@ -17,12 +17,12 @@ export class ReservationsConflictService {
         reservation.time.includes(time)
       )
     );
-    console.log('validateClassroom: ', validateClassroom);
-    console.log(reservationsClassroom);
+    // console.log('validateClassroom: ', validateClassroom);
+    // console.log(reservationsClassroom);
 
 
     if (reservationsClassroom.some(timeItem => data.time.every(t => timeItem.time.includes(t))) || validateClassroom.length > 0) {
-      console.log('Classroom: ', reservationsClassroom);
+      // console.log('Classroom: ', reservationsClassroom);
       throw new HttpException('A sala já foi reservada para esse horário.', HttpStatus.CONFLICT);
     }
 
@@ -34,8 +34,8 @@ export class ReservationsConflictService {
         reservation.time.includes(time)
       )
     );
-    console.log('validateClass: ', validateClass);
-    console.log('Class: ', reservationsClass);
+    // console.log('validateClass: ', validateClass);
+    // console.log('Class: ', reservationsClass);
 
     if (reservationsClass.some(timeItem => data.time.every(t => timeItem.time.includes(t))) || validateClass.length > 0) {
       throw new HttpException('A Turma já possui reserva nesse horário.', HttpStatus.CONFLICT);
@@ -49,8 +49,8 @@ export class ReservationsConflictService {
         reservation.time.includes(time)
       )
     );
-    console.log('validateUser: ', validateUser);
-    console.log('User: ', reservationsUser);
+    // console.log('validateUser: ', validateUser);
+    // console.log('User: ', reservationsUser);
 
     if (reservationsUser.some(timeItem => data.time.every(t => timeItem.time.includes(t))) || validateUser.length > 0) {
       throw new HttpException('O Professor já possui reserva nesse horário.', HttpStatus.CONFLICT);
