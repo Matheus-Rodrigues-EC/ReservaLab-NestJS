68cf7b870abf8fa3662d87f53af20ec3d49eb241
Matheus Rodrigues <68609506+Matheus-Rodrigues-EC@users.noreply.github.com>
2025-09-02 17:40:20 -0300

    Fix: Add cors domain

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173',], // front local + produção
+    origin: [
+      'http://localhost:5173',
+      'https://reserva-lab-development.vercel.app/',
+    ], // front local + produção
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // se usar cookies/autenticação
   });
